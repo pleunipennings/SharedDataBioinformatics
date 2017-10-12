f1b0388c585e8515154832641e6fc6cbd64f8423
Nathan ONeil <nnoneil22@gmail.com>
2017-10-12 11:58:04 -0700

    creation of Influ master file!

---
+++ 11AM_Influ_team/Group_Master_Script.R
@@ -0,0 +1,5 @@
+#### Master Script ####
+
+#Fig 2: location vs frequency CpG non-CpG.
+  #Plot num column vs MeanFreq column in a scatterplot. The points should be colored
+  #depending on whether they are Cpg / not CpG.
