163a5d88813185686f664702066dd974b080c164
Albert Wong <albertewong@gmail.com>
2017-10-12 00:53:43 -0700

    viewing the practice file

--- TeamNtropy.R
+++ TeamNtropy.R
@@ -2,4 +2,13 @@
 
 #function to : To create function that will chart
 #   the location of the mutation (x-axis) vs frequency of mutations (y-axis). 
-setwd()
+
+#set directory to where "OverviewSelfCoeff_BachelerFilter.csv" is
+setwd("c:/Users/alber/Downloads")
+
+#read file
+file<-read.csv("OverviewSelCoeff_BachelerFilter(1).csv")
+
+head(file)
+tail(file)
+summary(file)
