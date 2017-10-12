318a4ff81c1bcb426ec912d5b34836cd4785abea
amir jaberi <amirjaberi66@gmail.com>
2017-10-12 12:11:12 -0700

    #test

--- 11AM_Influ_team/Group_Master_Script.R
+++ 11AM_Influ_team/Group_Master_Script.R
@@ -3,3 +3,5 @@
 #Fig 2: location vs frequency CpG non-CpG.
   #Plot num column vs MeanFreq column in a scatterplot. The points should be colored
   #depending on whether they are Cpg / not CpG.
+
+#TEST 
