614ba77ac02b148533c8fc46ddfab6bc2e48893f
Gordon <basscswork@gmail.com>
2017-10-11 20:43:27 -0700

    got one CI point to work

--- night_crew_test.R
+++ night_crew_test.R
@@ -90,7 +90,9 @@ graph + geom_boxplot() +
 #building the subset plot
 ggplot(aes(factor(WTnt), MeanFreq), data = noncpGdata)+
     geom_jitter(col = "red") +
-    geom_errorbar(aes(ymin = lowerConf, ymax = upperConf), width = 0.2)
+    geom_errorbar(aes(ymin = median(lowerConf), ymax = median(upperConf), width = 0.2))+
+    geom_point(aes('a',median(c(median(lowerConf),median(upperConf)))))
+
 ggplot(aes(factor(WTnt), MeanFreq), data = cpGdata)+
     geom_jitter(color ="blue")
 
