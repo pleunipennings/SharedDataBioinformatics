50bbebef673c7471de8d79b53e07cbb92141bc4e
Albert Wong <albertewong@gmail.com>
2017-10-12 12:16:55 -0700

    code to examine data file

---
+++ function_mutfreq_vs_location_NTROPY/albert_function_chart_mut_freq_vs_loc.R
@@ -0,0 +1,10 @@
+#Albert's code for chart function
+
+#set directory and open data faile
+setwd("C:/Users/alber/Downloads")
+data<-read.csv("OverviewSelCoeff_BachelerFilter.csv")
+
+#examination of data
+head(data)
+tail(data)
+summary(data)
