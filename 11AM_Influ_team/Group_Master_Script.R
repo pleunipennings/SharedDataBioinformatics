26580d3d282cadf5c3cd5adca27567cac0a54ecb
Nathan ONeil <nnoneil22@gmail.com>
2017-10-17 12:55:55 -0700

    12:55 nate updated master script

--- 11AM_Influ_team/Group_Master_Script.R
+++ 11AM_Influ_team/Group_Master_Script.R
@@ -1,10 +1,34 @@
 #### Master Script ####
 
+#TEST 
+
 #Fig 2: location vs frequency CpG non-CpG.
   #Plot num column vs MeanFreq column in a scatterplot. The points should be colored
   #depending on whether they are Cpg / not CpG.
 
 #remember [PULL > Comment > Commit > PUSH]
 
-#TEST 
+
+
+
+#### function to find CpG Islands ####
+
+CpG_finder <- function("namefasta" format(n)){
+    n = read.alignment("namefasta")
+    format = #look at format code for 
+    
+}
+
+#### function to plot CpG Islands ####
+CpG_plotter <- function(){
+    
+}
+
+#### notes and comments section ####
+#10-17-2017 nathan 12:53
+    #Formatted the group master script
+    # added sections
+    #
+
+
 
