3033cb704e1edef77788aaf5f05039706daaf239
Albert Wong <albertewong@gmail.com>
2017-10-18 14:29:49 -0700

    Completed function to chart mutation freq(x) vs location of chromosome(y) from data frame columns 2 vs 3. with axis labels and colored legend of points.

--- chart_function_mutfreq_vs_location_NTROPY/freq_mutation_vs_location_function.R
+++ chart_function_mutfreq_vs_location_NTROPY/freq_mutation_vs_location_function.R
@@ -1,1 +1,31 @@
 #script for Team NTROPY's function graphing Frequency of synonymous/nonsynonmous/other mutation vs location
+
+#set directory and open data file
+setwd("C:/Users/alber/Downloads")
+
+#read csv file into variable "data"
+data<-read.csv("OverviewSelCoeff_BachelerFilter.csv")
+
+#plot data mut freq vs location
+plot(data[,2],
+     data[,3],
+     col=data$TypeOfSite,
+     pch=c(data$TypeOfSite),
+     #Title label
+    main = "HIV Practice Data",
+    #x axis label
+    xlab ="Location on Sequence", 
+    #y axis label
+    ylab ="Mean Frequency of Mutation"
+)
+
+
+#add legend in top right corner
+legend("topright", 
+       #names of each category
+       legend = levels(data$TypeOfSite), 
+       #symbols matching dataframe's factors of data$TypeOfSite
+       pch=c(1,2,3,4,5),
+       #colors matching dataframe's factors of data$TypeOfSite
+       col=c(1,2,3,4,5)
+)
