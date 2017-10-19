dacdb4c814b68e2b9b428bcde701078830c532aa
Nathan ONeil <nnoneil22@gmail.com>
2017-10-18 18:39:46 -0700

    made csv files

--- 11AM_Influ_team/Group_Master_Script.R
+++ 11AM_Influ_team/Group_Master_Script.R
@@ -34,19 +34,31 @@ CpG_plotter <- function(){
 #10-17-2017 nathan 12:53
     #Formatted the group master script
     # added sections
-    #
+# pure Fasta format Upload
 HPIV1a = read.alignment("humanparainfluenzavirus1.fasta_pruned.mu.trim05", format = "fasta")
 HPIV1b = read.alignment("humanparainfluenzavirus1_F.fasta_pruned.mu.trim05", format = "fasta")
 HPIV1c = read.alignment("humanparainfluenzavirus1_HN.fasta_pruned.mu.trim05", format = "fasta")
 HPIV3a = read.alignment("humanparainfluenzavirus3.fasta_pruned.mu.trim05", format = "fasta")
 HPIV3b = read.alignment("humanparainfluenzavirus3_HN.fasta_pruned.mu.trim05", format = "fasta")
-#average size of each sample is 15,500
+#average size of each sample is 15,500~ approx
+# none of this worked!
+#       str(HPIV1a)
+#       as.table(HPIV1a, stringsAsFactors=FALSE)
+#       M <- as.table(read.alignment("clean_HPIV1.txt", format = "fasta"))
+#       M
 
+#       B <-read.csv("Clean_HPIV1.csv")
+#       B
+#       str(B)
+
+# I took each fasta file and converteded it into a csv tabulated format
+
+HPIV1aT = read.csv("HPIV1aT.csv")
+HPIV1bT = read.csv("HPIV1bT.csv")
+HPIV1cT = read.csv("HPIV1cT.csv")
+HPIV3aT = read.csv("HPIV3aT.csv")
+HPIV3bT = read.csv("HPIV3bT.csv")
+#average size of each sample is 15,500~ approx
 str(HPIV1a)
-as.table(HPIV1a, stringsAsFactors=FALSE)
-M <- as.table(read.alignment("clean_HPIV1.txt", format = "fasta"))
-M
+head(HPIV1a)
 
-B <-read.csv("Clean_HPIV1.csv")
-B
-str(B)
