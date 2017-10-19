8afe7c89d1be21b08d4dba17e3d35647a78e1a39
Avery Chua <achua@mail.sfsu.edu>
2017-10-19 09:44:31 -0700

    - Loading appropriate libraries - deleting unused code - format code

--- DrasticAAChange9AM/HasansCode.R
+++ DrasticAAChange9AM/HasansCode.R
@@ -1,54 +1,54 @@
-#testing push and commit
-setwd("/Users/hasansulaeman/Google Drive/Bioinformatics/InfluenzaVirus")
-seqs = read.dna("InfluenzaAvirus_HA_H1N1.fasta.mu.fasta", format = "fasta", as.character=TRUE)
-a=nrow(seqs)
-b=ncol(seqs)
+#setting up enviroment
+
+  library(seqinr)
+  library(Biostrings)
+  library(ape)
+
+#reading files and setting data  
+  seqs = read.dna("InfluenzaAvirus_HA_H1N1.fasta.mu.fasta", format = "fasta", as.character=TRUE)
+  a=nrow(seqs)
+  b=ncol(seqs)
 
 # Let's establish the new data frame
 # Number of columns is arbitrary. I just want to print the WT sequence down a column
-df=data.frame(matrix(nrow=b, ncol=7)) 
-names(df)=c("WTseq", "Mutseq", "WTAA", "MutAA", "WTcat", "Mutcat", "DrasticAA")
+  df=data.frame(matrix(nrow=b, ncol=7)) 
+  names(df)=c("WTseq", "Mutseq", "WTAA", "MutAA", "WTcat", "Mutcat", "DrasticAA")
 
 # We're defining the wild type sequence as the most common sequence
-for(i in 0:(b-1)){
-  x=table(seqs[,i+1])
-  y=names(x[which.max(x)])
-  df[i+1,1]=y
-}
+  for(i in 0:(b-1)){
+    x=table(seqs[,i+1])
+    y=names(x[which.max(x)])
+    df[i+1,1]=y
+  }
 
 # Translate the WT RNA sequence to AA sequence
 
-curSeq <- translate(paste(df[,1], sep=" "),NAstring="X", ambiguous=FALSE, sens="F")
-count <- 0
-
-for(i in 1:length(curSeq)){ # incrementing down sequence by 3 (needs work)
-        count <- count + 1
-        df[count,]$WTAA <- curSeq[i]
-        count <- count + 1
-        df[count,]$WTAA <- curSeq[i]
-        count <- count + 1
-        df[count,]$WTAA <- curSeq[i]
-}
-
-
-
-df[,3]=seqinr::translate(paste(df[,1], sep=" "),
-                         NAstring="X", ambiguous=FALSE, sens="F")
+  curSeq <- translate(paste(df[,1], sep=" "),NAstring="X", ambiguous=FALSE, sens="F")
+  count <- 0
+  
+  for(i in 1:length(curSeq)){ # incrementing down sequence by 3 (needs work)
+          count <- count + 1
+          df[count,]$WTAA <- curSeq[i]
+          count <- count + 1
+          df[count,]$WTAA <- curSeq[i]
+          count <- count + 1
+          df[count,]$WTAA <- curSeq[i]
+  }
 
 #Defining AA changes
-pos <- "R|H|K"
-neg <- "D|E"
-unc <- "S|T|N|Q"
-spe <- "C|U|G|P"
-hyd <- "A|I|L|F|M|W|Y|V"
-amCat <- function(AA){
-  if(regexpr(pos, AA) > 0){ return(0) }
-  if(regexpr(neg, AA) > 0){ return(1) }
-  if(regexpr(unc, AA) > 0){ return(2) }
-  if(regexpr(spe, AA) > 0){ return(3) }
-  if(regexpr(hyd, AA) > 0){ return(4) }
-  return(5)
-}
+  pos <- "R|H|K"
+  neg <- "D|E"
+  unc <- "S|T|N|Q"
+  spe <- "C|U|G|P"
+  hyd <- "A|I|L|F|M|W|Y|V"
+  amCat <- function(AA){
+    if(regexpr(pos, AA) > 0){ return(0) }
+    if(regexpr(neg, AA) > 0){ return(1) }
+    if(regexpr(unc, AA) > 0){ return(2) }
+    if(regexpr(spe, AA) > 0){ return(3) }
+    if(regexpr(hyd, AA) > 0){ return(4) }
+    return(5)
+  }
 
 # Categorizing the WT amino acids
 for(j in 1:b){
