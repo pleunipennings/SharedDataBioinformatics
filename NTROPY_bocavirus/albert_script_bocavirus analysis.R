b2fdda8b4f9c9ec0ab8188f72b32373badffe4cc
Albert Wong <albertewong@gmail.com>
2017-10-19 13:02:19 -0700

    Further preparation of fasta file for data frame

--- NTROPY_bocavirus/albert_script_bocavirus analysis.R
+++ NTROPY_bocavirus/albert_script_bocavirus analysis.R
@@ -10,15 +10,21 @@ bocaNS1seqsAli<-read.alignment("HumanBocavirus1_NS1.fasta_pruned.mu.trim05", for
 bocaNS1seqsAli
 class(bocaNS1seqsAli)
 tail(bocaNS1seqsAli)
-bocaNS1seqsAli[3]
+bocaNS1seqsAli
+bocaNS1seqs
+bocaNS1seqsAli$seq[[1]]
 
-cons<-consensus(bocaNS1seqsAli[[1:31]])
+#CANT MAKE IT WORK returns error "Error: $ operator is invalid for atomic vectors"
+cons<-consensus(bocaNS1seqsAli, format="fasta")
 
 #use read.dna to get the data in matrix form, this makes it easier to count
-NAseqsDNA<-read.dna("NA_alignment.fasta", format = "fasta",as.character=TRUE)
+bocaNS1seqsDNA<-read.dna("HumanBocavirus1_NS1.fasta_pruned.mu.trim05", format = "fasta",as.character=TRUE)
+bocaNS1seqsDNA
+class(bocaNS1seqsDNA)
+summary(bocaNS1seqsDNA)
 
 #Now you can use length and which and the == operator to count the number of sequences with the consensus nucleotide
-numCons<-length(which(NAseqsDNA[,1]==cons[1]))
+numCons<-length(which(bocaNS1seqsDNA[,1]==cons[1]))
 #Here you should use a function to determine the transition from the consensus. For now, I just put "g", but it's better to use a function! 
 numTrans<-length(which(NAseqsDNA[,1]=="g"))
 
