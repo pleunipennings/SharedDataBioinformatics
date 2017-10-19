d6904f44111d849e7c6a93b100fa7c558c1b28e9
Jackylo427 <jackylo427@gmail.com>
2017-10-18 23:09:19 -0700

    Jacky

---
+++ DengueVirus9AM/JACKY_HIV_CODE.R
@@ -0,0 +1,35 @@
+read.csv("HIVDATA.CSV") ->HIV
+
+HIV
+
+HIV$WTnt
+
+paste(HIV$WTnt, collapse = '') -> STRING
+
+STRING
+
+
+gregexpr(pattern ='tg',STRING ) -> PEN
+PEN
+
+BELL <- data.frame(matrix(unlist(PEN)))
+BELL
+
+HIV$JOHN<- 0
+
+HIV[BELL[,1],"JOHN"] <- BELL[,1]
+
+
+
+
+
+##  HIV[2,"JOHN"] <- 1
+
+
+gregexpr(pattern ='ca',HUH) -> MAP
+MA
+
+
+
+
+
