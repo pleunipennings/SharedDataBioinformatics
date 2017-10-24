1ab94e5da0a180dcf299ddeec71bc4c355bebe50
Jackylo427 <jackylo427@gmail.com>
2017-10-24 09:14:30 -0700

    added a legend

--- OWL/Graph.R
+++ OWL/Graph.R
@@ -1,11 +1,13 @@
-setwd("~/Desktop")
 read.csv("OverviewSelCoeff_BachelerFilter.csv")->HIVdata
 print(HIVdata)
 
+
 #Identify noCpG sites
-which(HIVdata[,13]=="0")->noCpG
+#column might be 13 of 15, so change accordingly 
+which(HIVdata[,15]=="0")->noCpG
+print(noCpG)
 #Identify CpG sites
-which(HIVdata[,13]=="1")->CpG
+which(HIVdata[,15]=="1")->CpG
 
 #Identify mean frequency in association with noCpG sites
 HIVdata[noCpG,3]->Freq
@@ -13,12 +15,14 @@ HIVdata[noCpG,3]->Freq
 HIVdata[CpG,3]->Freq2
 
 
-
 #Making a plot of mean frequency vs. CpG/nonCpG location
 plot(noCpG, Freq, col="red", main="Mean Frequency vs. CpG/noCpG location", xlab = "Location", ylab = "Mean Frequency")
 #Overlapping the two graphs
 par(new = TRUE )
 plot(CpG, Freq2,col="blue",xaxt='n',yaxt='n', ann = FALSE)
+
+
+legend("topright",c("noCpG","CpG"),lty = c(1,1),lwd=4, col=c("red","blue"),bty="n")
 
 
 
