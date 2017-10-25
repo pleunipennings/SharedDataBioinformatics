8c1113f7908b47ddec31f09597498a6ea2acd92d
amir jaberi <amirjaberi66@gmail.com>
2017-10-25 16:47:11 -0700

    new codes 10_25_17 meeting fo consensus

--- 11AM_Influ_team/consensus coding 10_24_17.R
+++ 11AM_Influ_team/consensus coding 10_24_17.R
@@ -7,15 +7,36 @@ aln2 <- aln[[1]]
 
 aln<-read.alignment("BKpolyomavirus_VP1.fasta.mu.trim05.txt", format = "fasta", forceToLower = TRUE)
 
-aln<-read.dna("BKpolyomavirus_VP1.fasta.mu.trim05.txt")
+#aln<-read.dna("BKpolyomavirus_VP1.fasta.mu.trim05.txt")
 
 aln<-as.matrix.alignment(aln)
 # Generate consensus
 con<-seqinr::consensus(aln)
-print(con$seq)
+con
 
-##### from nate for data.frame() #####
-n <- data.frame(Pos = c(1:length(seqinr::consensus(HPIV1a))),
-                WTnt = (seqinr::consensus(HPIV1a)),
-                Trans = c("frequency()")
-)#closes data.frame
+
+#store lenght of the sequence into a variable called X
+X<-length(aln)
+X
+
+#counts the number of As,Cs,Gs and Ts in the sequence 
+a<-count(aln,1)
+
+#calculates frequency of A's 
+a_freq<-(a[1]/X)
+
+c_freq<-(a[2]/X)
+
+g_freq<-(a[3]/X)
+
+t_freq<-(a[4]/X)
+
+t_freq
+g_freq
+c_freq
+a_freq
+
+
+
+
+
