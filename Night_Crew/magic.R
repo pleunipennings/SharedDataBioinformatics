4257931e704552cf1269c034c259a6a80ecaf915
Vcaudill <vcaudill@mail.sfsu.edu>
2017-10-26 10:00:47 -0700

    nonref

--- Night_Crew/magic.R
+++ Night_Crew/magic.R
@@ -4,15 +4,13 @@ library(seqinr)
 
 bk <- read.fasta("bk.txt")
 
-# reference sequence
-ref <- bk[[1]]
 
 # dataframe columns
 num <- c(1:1089)
 wtnt <- c()
-freq <- c()
+MeanFreq <- c()
 
-# for freq calculation later
+# for MeanFreq calculation later
 absfreq <- c(rep(0,1089))
 totalcount <- c(rep(0,1089))
 
@@ -26,75 +24,77 @@ nuc <- c()
 
 # same as line 20 comment
 for (i in 1:length(bk)) {
-  sequence <- bk[[i]]
-  for (j in 1:length(sequence)) {
-    if (sequence[j] == 'a') {acount[j] = acount[j] + 1}
-    if (sequence[j] == 'g') {gcount[j] = gcount[j] + 1}
-    if (sequence[j] == 'c') {ccount[j] = ccount[j] + 1}
-    if (sequence[j] == 't') {tcount[j] = tcount[j] + 1}
-  }
+    sequence <- bk[[i]]
+    for (j in 1:length(sequence)) {
+        if (sequence[j] == 'a') {acount[j] = acount[j] + 1}
+        if (sequence[j] == 'g') {gcount[j] = gcount[j] + 1}
+        if (sequence[j] == 'c') {ccount[j] = ccount[j] + 1}
+        if (sequence[j] == 't') {tcount[j] = tcount[j] + 1}
+    }
 }
 
 # assigns wtnt based on most frequent nucleotide across all sequences per position
 for (j in 1:length(sequence)) {
-  nuc[j] <- max(c(acount[j],gcount[j],ccount[j],tcount[j]))
-  if (max(nuc[j]) == acount[j]) {wtnt[j] <- 'a'}
-  if (max(nuc[j]) == gcount[j]) {wtnt[j] <- 'g'}
-  if (max(nuc[j]) == ccount[j]) {wtnt[j] <- 'c'}
-  if (max(nuc[j]) == tcount[j]) {wtnt[j] <- 't'}
-  nuc <- c()
+    nuc[j] <- max(c(acount[j],gcount[j],ccount[j],tcount[j]))
+    if (max(nuc[j]) == acount[j]) {wtnt[j] <- 'a'}
+    if (max(nuc[j]) == gcount[j]) {wtnt[j] <- 'g'}
+    if (max(nuc[j]) == ccount[j]) {wtnt[j] <- 'c'}
+    if (max(nuc[j]) == tcount[j]) {wtnt[j] <- 't'}
+    nuc <- c()
 }
 
 # gives absolute totals to be used for frequency calculation
 for (i in 1:length(bk)) {
-  sequence <- bk[[i]]
-  for (j in 1:length(sequence)){
-    if (wtnt[j] == 'a') {
-      if (sequence[j] == 'g') {
-        absfreq[j] <- absfreq[j] + 1
-        totalcount[j] <- totalcount[j] + 1
-      }
-      else if (sequence[j] == 'a') {
-        totalcount[j] <- totalcount[j] + 1
-      }
+    sequence <- bk[[i]]
+    for (j in 1:length(sequence)){
+        if (wtnt[j] == 'a') {
+            if (sequence[j] == 'g') {
+                absfreq[j] <- absfreq[j] + 1
+                totalcount[j] <- totalcount[j] + 1
+            }
+            else if (sequence[j] == 'a') {
+                totalcount[j] <- totalcount[j] + 1
+            }
+        }
+        if (wtnt[j] == 'g') {
+            if (sequence[j] == 'a') {
+                absfreq[j] <- absfreq[j] + 1
+                totalcount[j] <- totalcount[j] + 1
+            }
+            else if (sequence[j] == 'g') {
+                totalcount[j] <- totalcount[j] + 1
+            }
+        }
+        if (wtnt[j] == 'c') {
+            if (sequence[j] == 't') {
+                absfreq[j] <- absfreq[j] + 1
+                totalcount[j] <- totalcount[j] + 1
+            }
+            else if (sequence[j] == 'c') {
+                totalcount[j] <- totalcount[j] + 1
+            }
+        }
+        if (wtnt[j] == 't') {
+            if (sequence[j] == 'c') {
+                absfreq[j] <- absfreq[j] + 1
+                totalcount[j] <- totalcount[j] + 1
+            }
+            else if (sequence[j] == 't') {
+                totalcount[j] <- totalcount[j] + 1
+            }
+        }
     }
-    if (wtnt[j] == 'g') {
-      if (sequence[j] == 'a') {
-        absfreq[j] <- absfreq[j] + 1
-        totalcount[j] <- totalcount[j] + 1
-      }
-      else if (sequence[j] == 'g') {
-        totalcount[j] <- totalcount[j] + 1
-      }
-    }
-    if (wtnt[j] == 'c') {
-      if (sequence[j] == 't') {
-        absfreq[j] <- absfreq[j] + 1
-        totalcount[j] <- totalcount[j] + 1
-      }
-      else if (sequence[j] == 'c') {
-        totalcount[j] <- totalcount[j] + 1
-      }
-    }
-    if (wtnt[j] == 't') {
-      if (sequence[j] == 'c') {
-        absfreq[j] <- absfreq[j] + 1
-        totalcount[j] <- totalcount[j] + 1
-      }
-      else if (sequence[j] == 't') {
-        totalcount[j] <- totalcount[j] + 1
-      }
-    }
-  }
 }
 
 # calculates frequency as percentage
 for (i in 1:length(absfreq)) {
-  freq[i] <- absfreq[i] / totalcount[i]
+    MeanFreq[i] <- absfreq[i] / totalcount[i]
 }
 
+
+
 # creates dataframe containing all data
-bk_data <- data.frame(num,wtnt,freq)
+bk_data <- data.frame(num,wtnt,MeanFreq)
 
 # outputs data to file
 write.csv(bk_data,"bk_data.csv")
@@ -102,9 +102,5 @@ save(bk_data,file="bk_data.Rda")
 load("bk_data.Rda")
 
 #check plot
-plot(bk_data$freq + 0.01, log="y", col=c(1,2,3))
-#1 black fist aa in codon, 2 red 2nd aa in codon, 3 green 3rd aa in codon
-
-
-plot(bk_data$freq + 0.01, log="y", col=c(1,2,3))
+plot(bk_data$MeanFreq + 0.01, log="y", col=c(1,2,3))
 #1 black fist aa in codon, 2 red 2nd aa in codon, 3 green 3rd aa in codon
