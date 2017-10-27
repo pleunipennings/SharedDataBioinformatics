48775acc19dde7b04a0bc4a225f51c3ed3a9691d
Albert Wong <albertewong@gmail.com>
2017-10-26 22:10:28 -0700

    updates

--- NTROPY_bocavirus/albert_script_bocavirus analysis.R
+++ NTROPY_bocavirus/albert_script_bocavirus analysis.R
@@ -1,6 +1,9 @@
-setwd("~/bioinformatics/HumanBocaVirus/HumanBocaVirus")
+setwd("~/bioinformatics/bioinformaticsproject")
 library(seqinr)
 library(ape)
+
+read.csv("OverviewSelCoeff_BachelerFilter(1).csv") -> sampledata
+head(sampledata)
 
 #boca NS1 stands for bocavirus NS1 gene
 bocaNS1seqs<-read.fasta("HumanBocavirus1_NS1.fasta_pruned.mu.trim05") 
@@ -15,22 +18,47 @@ class(bocaNS1seqsAli)
 #works when using con() instead of consensus()
 cons<-con(bocaNS1seqsAli)
 cons
+str(cons)
 
 #use read.dna to get the data in matrix form, this makes it easier to count
 bocaNS1seqsDNA<-read.dna("HumanBocavirus1_NS1.fasta_pruned.mu.trim05", format = "fasta",as.character=TRUE)
-bocaNS1seqsDNA
-bocaNS1seqsDNA[[17]]
-class(bocaNS1seqsDNA)
+str(bocaNS1seqsDNA)
+bocaNS1seqsDNA[31,]
+class(bocaNS1seqsDNA])
 summary(bocaNS1seqsDNA)
 
 #Now you can use length and which and the == operator to count the number of sequences with the consensus nucleotide
-numCons<-length(which(bocaNS1seqsDNA[,1]==cons[1]))
+
+BoNS1df<-data.frame("num"=c(1:5157), "WTnt"=cons)
+BoNS1df
+
+#use for loop to run through each location of 1 to 5157 nt
+numCons<-length(which(bocaNS1seqsDNA[,nt]==cons[,nt]))
+
 numCons
+
 #Here you should use a function to determine the transition from the consensus. For now, I just put "g", but it's better to use a function! 
-numTrans<-length(which(NAseqsDNA[,1]=="g"))
+numTrans<-length(which(bocaNS1seqsDNA[,1]=="g"))
+numTrans
 
-numTrans<-length(which(NAseqsDNA[,1]==transitionfunction(cons[1])))
+#function to run to see transition from consensus
+for (s in 1:5157){
+    BoNS1df$newcol<-cons
+}
+View(BoNS1df)
 
+meanfreq<-function(x){
+    if (cons[,1]!=bocaNS1seqsDNA[,1]) {
+        length(which(bocaNS1seqsDNA[,1]=="g"))
+    }
+    else {
+        if (bocaNS1seqsDNA[,1=="a"])
+    }
+}
 
+numTrans<-length(which(bocaNS1seqsDNA[,1]==transitionfunction(cons[1])))
+
+BoNS1df<-data.frame("num"=c(1:5157), "WTnt"=cons)
+BoNS1df
 
 
--- chart_function_mutfreq_vs_location_NTROPY/albert_HintsForReadingYourData.R
+++ chart_function_mutfreq_vs_location_NTROPY/albert_HintsForReadingYourData.R
@@ -12,14 +12,26 @@ bvseqsAli
 #consensus fxn to find the most common nucleotide
 cons<-consensus(bvseqsAli)
 cons
+class(cons)
 
 #use read.dna to get the data in matrix form, this makes it easier to count
 bvseqsDNA<-read.dna("HumanBocavirus1_NS1.fasta_pruned.mu.trim05", format = "fasta",as.character=TRUE)
-
+bvseqsDNA
+bvseqsDNA[,1]
 #Now you can use length and which and the == operator to count the number of sequences with the consensus nucleotide
-numCons<-length(which(NAseqsDNA[,1]==cons[1]))
+numCons<-length(which(bvseqsDNA[,1]==cons[1]))
+numCons
 #Here you should use a function to determine the transition from the consensus. For now, I just put "g", but it's better to use a function! 
-numTrans<-length(which(NAseqsDNA[,1]=="g"))
+numTrans<-length(which(bvseqsDNA[,1]=="a"))
+numTrans
+
+transition<-function(nuc){
+    if(nuc %in% c("a", "A")) {mutnuc="G"}
+    if(nuc %in% c("g","G")) {mutnuc="A"}
+    if(nuc %in% c("c","C")) {mutnuc="T"}
+    if(nuc %in% c("t","T")) {mutnuc="C"} 
+    return(mutnu)
+}
 
 numTrans<-length(which(NAseqsDNA[,1]==transitionfunction(cons[1])))
 
