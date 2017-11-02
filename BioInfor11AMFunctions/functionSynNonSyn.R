3175b751bdbd669b79e983dfb492a5fbe61fdc6f
Albert Wong <albertewong@gmail.com>
2017-11-02 13:00:26 -0700

    updates

---
+++ BioInfor11AMFunctions/functionSynNonSyn.R
@@ -0,0 +1,21 @@
+#  Note that this depends on the columns MUTAA and WTAA being already complete!
+
+functionSynNonSyn<-function(DF){
+    if (length(which(names(DF))=="MUTAA")==0){
+        print("Oh oh there is a problem. No MUTAA column!")
+        return(0)}
+    for (h in 1:nrow(DF)){
+        if(DF$MUTAA[h]== DF$WTAA[h]){
+            #   if(DF[h,"MUTAA"]== DF[h,"WTAA"]){
+            DF[h,"TypeOfSite"] = "syn"
+        }
+        if(DF[h,"MUTAA"] != DF[h,"WTAA"]){
+            if(DF[h,"MUTAA"]=="*"){
+                DF[h,"TypeOfSite"] = "nonsense"}
+            else {
+                DF[h,"TypeOfSite"] = "nonsyn"
+            }
+        }
+    }
+    DF$TypeOfSite<-as.factor(DF$TypeOfSite)
+}
