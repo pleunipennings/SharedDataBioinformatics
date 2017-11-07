241c824fd750a2b9ba859d514bf83ce33750302a
Christen Kinney <christenkinney@gmail.com>
2017-11-06 18:45:21 -0800

    Added annotations and updated names

--- 9AMBioinformaticsFunctions/PlotCpGsites.R
+++ 9AMBioinformaticsFunctions/PlotCpGsites.R
@@ -1,21 +1,24 @@
-read.csv("OverviewSelCoeff_BachelerFilter.csv")->HIVdata
-print(HIVdata)
+#This function makes a plot that shows the location of CpG and noCpG sites vs. Frequency.
+#This function assumes that you have num, freq, and CPG are already in your dataframe.
+
+#Team: Christen, Rima, Nicole, and Kellen.
+#Would also like to thank Scott and Pleuni for their help with making corrections to our code. 
 
 #Identify noCpG sites
-which(HIVdata$makesCpG=="0")->noCpG
+which(VIRUS_DATA$CPG=="0")->noCpG
 print(noCpG)
 #Identify CpG sites
-which(HIVdata$makesCpG=="1")->CpG
+which(VIRUS_DATA$CPG=="1")->CpG
 
 #Identify mean frequency in association with noCpG sites
-HIVdata[noCpG,"MeanFreq"]->Freq
+VIRUS_DATA[noCpG,"freq"]->Freq
 #Identify mean frequency in association with CpG sites
-HIVdata[CpG,"MeanFreq"]->Freq2
+VIRUS_DATA[CpG,"freq"]->Freq2
 
 
 
 #Making a plot of mean frequency vs. CpG/nonCpG location
-plot(noCpG, Freq+0.0001, ylim=c(0.0001,0.5), log = "y", col="black",pch=21.25, bg=rgb(1,0,0,0.5), main="CpG/noCpG location vs. Mean Frequency", xlab = "Location", ylab = "Mean Frequency")
+plot(noCpG, Freq+0.0001, ylim=c(0.0001,0.5), col="black",pch=21.25, bg=rgb(1,0,0,0.5), main="CpG/noCpG location vs. Mean Frequency", xlab = "Location", ylab = "Mean Frequency")
  
 #Overlapping the two graphs
 points(CpG, Freq2+0.0001, col="black",pch=21.25, bg=rgb(0,0,1,0.5))
