f6f564c0b61141ccdf24ef01ae03e817116af7da
efryer333 <32182478+efryer333@users.noreply.github.com>
2017-11-07 13:07:23 -0800

    The drastic AA change officially been wrapped into a function

--- 9AMBioinformaticsFunctions/DrasticAAChange.R
+++ 9AMBioinformaticsFunctions/DrasticAAChange.R
@@ -11,12 +11,8 @@
 # This function assumes that the user has installed the packages: seqinr, Biostrings and ape.
 # The file pathway in line 21 needs to be changed to adjust for the data being analyzed
 
-#setting up new column necessary to run function
 
-df$Mutcat<-NA
-df$WTcat<-NA
-df$bigAAchange<-NA
-
+DrasticChange <- function(df){
 
 #Defining AA changes
 pos <- "R|H|K"
@@ -25,44 +21,36 @@ unc <- "S|T|N|Q"
 spe <- "C|U|G|P"
 hyd <- "A|I|L|F|M|W|Y|V"
 amCat <- function(AA){
-    if(regexpr(pos, AA) > 0){ return("pos") }
-    if(regexpr(neg, AA) > 0){ return("neg") }
-    if(regexpr(unc, AA) > 0){ return("unc") }
-    if(regexpr(spe, AA) > 0){ return("spe") }
-    if(regexpr(hyd, AA) > 0){ return("hyd") }
-    return(5)
+  if(regexpr(pos, AA) > 0){ return("pos") }
+  if(regexpr(neg, AA) > 0){ return("neg") }
+  if(regexpr(unc, AA) > 0){ return("unc") }
+  if(regexpr(spe, AA) > 0){ return("spe") }
+  if(regexpr(hyd, AA) > 0){ return("hyd") }
+  return(5)
 }
-# Categorizing the WT amino acids
-for(j in 1:nrow(df)){
+
+  df$Mutcat <- 0  #setting up new column necessary to run function
+  df$WTcat <- 0
+  df$bigAAchange <- 0
+ 
+  for(j in 1:b){
     df[j,]$WTcat=amCat(df[j,]$WTAA)
-}
-DrasticChange <- function(df){
-    for(h in 1:nrow(df)){
-        if (df[h,]$WTcat==df[h,]$Mutcat){
-            df[h,]$bigAAchange= 0                      # If WT AA category = Mut AA Category, no drastic change
-        }
-        if (df[h,]$WTcat!=df[h,]$Mutcat){
-            df[h,]$bigAAchange= 1                      # If WT AA category =/= Mut AA Category, yes drastic change
-        }
-    }
-    return(df)
-}
-for(j in 1:b){                                              
-    df[j,]$Mutcat=amCat(df[j,]$MUTAA)                                    # Categorizing the AA 
+  }
+  
+for(j in 1:nrow(df)){                                              
+  df[j,]$Mutcat=amCat(df[j,]$MUTAA)                                    # Categorizing the AA 
 }
 
 # Function to compare DrasticAAChanges
-DrasticChange <- function(df){
-    for(h in 1:nrow(df)){
-        if (df[h,]$WTcat==df[h,]$Mutcat){
-            df[h,]$bigAAchange= 0                      # If WT AA category = Mut AA Category, no drastic change
-        }
-        if (df[h,]$WTcat!=df[h,]$Mutcat){
-            df[h,]$bigAAchange= 1                      # If WT AA category =/= Mut AA Category, yes drastic change
-        }
-    }
-    return(df)
-}
 
-df <- DrasticChange(df) #runs the Drastic Change Function
-df
+  for(h in 1:nrow(df)){
+    if (df[h,]$WTcat==df[h,]$Mutcat){
+      df[h,]$bigAAchange= 0                      # If WT AA category = Mut AA Category, no drastic change
+    }
+    if (df[h,]$WTcat!=df[h,]$Mutcat){
+      df[h,]$bigAAchange= 1                      # If WT AA category =/= Mut AA Category, yes drastic change
+    }
+  }
+  return(df)
+}
+df<-DrasticChange(df)
