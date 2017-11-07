7f82a3ddbb2a6416ae597b7eb3af4c7c15fbfe4a
pleunipennings <pspennings@gmail.com>
2017-11-07 12:10:26 -0800

    changes to non syn

--- BioInfor11AMFunctions/functionSynNonSyn.R
+++ BioInfor11AMFunctions/functionSynNonSyn.R
@@ -1,13 +1,18 @@
 #  Note that this depends on the columns MUTAA and WTAA being already complete!
 
 functionSynNonSyn<-function(dengue_df){
-    if (length(which(names(dengue_df))=="MUTAA")==0){
+    
+    #Create "TypeOfSite" column if not already
+    if (length(which(names(dengue_df)=="TypeOfSite"))==0){
+        dengue_df$TypeOfSite=0}
+    
+    if (length(which(names(dengue_df)=="MUTAA"))==0){
         print("Oh oh there is a problem. No MUTAA column!")
         return(0)}
     for (h in 1:nrow(dengue_df)){
         if(dengue_df$MUTAA[h]== dengue_df$WTAA[h]){
             #   if(dengue_df[h,"MUTAA"]== dengue_df[h,"WTAA"]){
-            dengue_df[h,"TypeOfSite"] = "syn"
+            dengue_df[h,"TypeOfSite"] = "syn"}
         if(dengue_df[h,"MUTAA"] != dengue_df[h,"WTAA"]){
             if(dengue_df[h,"MUTAA"]=="*"){
                 dengue_df[h,"TypeOfSite"] = "nonsense"}
@@ -18,4 +23,3 @@ functionSynNonSyn<-function(dengue_df){
     }
     dengue_df$TypeOfSite<-as.factor(dengue_df$TypeOfSite)
     }
-}
