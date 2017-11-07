1d2ca04182a158955a33e51271fd2555418c141c
rlm2013 <32551329+rlm2013@users.noreply.github.com>
2017-11-06 20:28:05 -0800

    Update SynNonNonsense_final_9am.R

--- 9AMBioinformaticsFunctions/SynNonNonsense_final_9am.R
+++ 9AMBioinformaticsFunctions/SynNonNonsense_final_9am.R
@@ -1,15 +1,15 @@
 #9am
 #function for syn/non/nonsence
 #function for syn/non/nonsence
-for (h in nrow(df)){
-  if(df[h,"MUTAA"]== df[h,"WTAA"]){
-    df[h,"TypeOfSite"] = "syn"
+for (h in nrow(df)){ #looks at each row in the dataframe df
+  if(df[h,"MUTAA"]== df[h,"WTAA"]){ #if the value in the MUTAA column for the row of interest is equal to the value in the WTAA of the same row
+    df[h,"TypeOfSite"] = "syn" #then insert "syn" for the value in the TypeOfSite column for this row
   }
-  if(df[h,"MUTAA"] != df[h,"WTAA"]){
-    if(df[h,"MUTAA"]=="*"){
-      df[h,"TypeOfSite"] = "nonsense"}
+  if(df[h,"MUTAA"] != df[h,"WTAA"]){ #if the value in the MUTAA column for the row of interest is NOT equal to the value in the WTAA of the same row
+    if(df[h,"MUTAA"]=="*"){ #and it the MUTAA value equal "*"
+      df[h,"TypeOfSite"] = "nonsense"} #then insert "nonsense" for the value in the TypeOfSite column for this row
     else {
-      df[h,"TypeOfSite"] = "nonsyn"
+      df[h,"TypeOfSite"] = "nonsyn" #else insert "nonsyn" for the value in the TypeOfSite column for this row
     }
   }
 }
